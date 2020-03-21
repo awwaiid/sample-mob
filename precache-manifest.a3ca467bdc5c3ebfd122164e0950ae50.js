10c57fb03a28695661552a58c3756fc2329d86d0
Brock Wilcox <awwaiid@thelackthereof.org>
2020-03-21 16:44:46 -0400

    deploy

---
+++ precache-manifest.a3ca467bdc5c3ebfd122164e0950ae50.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09331a3992df538022c",
+    "url": "/css/app.2bc41efa.css"
+  },
+  {
+    "revision": "a0fe28d171eb513a2781",
+    "url": "/css/leaders.d500343f.css"
+  },
+  {
+    "revision": "bd21c21dfe2f6f498e56",
+    "url": "/css/synth.477c03c7.css"
+  },
+  {
+    "revision": "8ead7831d2eb7cdc496fd7b833728a48",
+    "url": "/img/mizik-logo-02.8ead7831.png"
+  },
+  {
+    "revision": "944eff3ef3100f3f0f618e27dab55708",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ced582252b22d19b642",
+    "url": "/js/about.f0def901.js"
+  },
+  {
+    "revision": "a09331a3992df538022c",
+    "url": "/js/app.31fd8c79.js"
+  },
+  {
+    "revision": "e554556fe65bcaeb8217",
+    "url": "/js/chunk-vendors.94ad7707.js"
+  },
+  {
+    "revision": "a0fe28d171eb513a2781",
+    "url": "/js/leaders.62bce293.js"
+  },
+  {
+    "revision": "452166292fd7be3154ae",
+    "url": "/js/leaders~synth.ad39e5dd.js"
+  },
+  {
+    "revision": "bd21c21dfe2f6f498e56",
+    "url": "/js/synth.5993ba51.js"
+  },
+  {
+    "revision": "1fff1ec90ddb25b4f9cadbee5ef6fb86",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b86904ef411bc5d1a29f457ede36bf9e",
+    "url": "/mizik-20200202.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "aac0a526bd9f691dcaa8d6bd2b22ba67",
+    "url": "/synth-20200119.png"
+  }
+]);
